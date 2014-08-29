33a05dc9a4336f8b9bc8eb80a11ea1dbabed1c1b
schevalier <chevalier.syl20@gmail.com>
2014-08-29 14:41:42 +0200

    Dotfiles rework

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 files=`ls -1 ~/dotfiles/symlinks`
 for file in $files; do
     echo "Creating symlink for $file"
-    echo ~/dotfiles/symlinks/$file
     ln -sf ~/dotfiles/symlinks/$file ~/.$file
 done
